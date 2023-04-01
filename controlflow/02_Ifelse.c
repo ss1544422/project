09834bf9c8c59865a62d2b38cf7d6d70d9f564ea
ss1544422 <128373917+ss1544422@users.noreply.github.com>
2023-04-02 00:11:21 +0530

    Add files via upload

---
+++ controlflow/02_Ifelse.c
@@ -0,0 +1,36 @@
+/*********************************************************************************************
+ * © 2022 RuggedBOARRD. - All Rights Reserved. Permission to use, modify, copy, and distribute
+ * this source code, object code, or executable code (collectively, Software), is granted only
+ * under the terms of a valid written license agreement with RuggedBOARD. Unauthorized copying
+ * or other use of the Software is strictly prohibited.  Software is owned by and constitutes
+ * the proprietary works, trade secrets, and copyrights of Embitel or its licensors.
+ * For further information, contact community.ruggedboard.com / info@ruggedboard.com
+ *
+ * Application: 
+ * Brief: 
+ * Author: Suraj Singh
+ * Title: Program Engineer
+ * Last Modified Date: 30.03.2023
+ *
+/*********************************************************************************************/
+
+// Header File
+#include <stdio.h>
+
+int main() {
+    int num;
+    
+    printf("Enter a number: \n");
+    scanf("%d", &num);
+    
+    if (num > 20) {
+        printf("The number is Greater than 20.\n");
+    } else {
+        printf("The number is Lesser than 20.\n");
+    }
+    
+    return 0;
+}
+
+
+// Program End
