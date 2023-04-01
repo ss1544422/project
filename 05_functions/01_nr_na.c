df9b1c08c656f491c4b2b761de406b2bc64b722e
ss1544422 <128373917+ss1544422@users.noreply.github.com>
2023-04-02 00:20:50 +0530

    Add files via upload

---
+++ 05_functions/01_nr_na.c
@@ -0,0 +1,36 @@
+/*********************************************************************************************
+ * © 2022 RuggedBOARRD. - All Rights Reserved. Permission to use, modify, copy, and distribute
+ * this source code, object code, or executable code (collectively, Software), is granted only
+ * under the terms of a valid written license agreement with RuggedBOARD. Unauthorized copying
+ * or other use of the Software is strictly prohibited.  Software is owned by and constitutes
+ * the proprietary works, trade secrets, and copyrights of Embitel or its licensors.
+ * For further information, contact community.ruggedboard.com / info@ruggedboard.com
+ *
+ * Application: 
+ * Brief: 
+ * Author: Suraj Singh
+ * Title: Program Engineer
+ * Last Modified Date: 01.04.2023
+ *
+/*********************************************************************************************/
+
+// Header File
+#include<stdio.h>
+
+void sum(void);
+
+int main ()
+{
+   sum ();
+   return 0;
+}
+void sum ()
+{
+   int a,b,c;
+   printf("enter 2 numbers:\n");
+   scanf ("%d%d", &a, &b);
+   c = a+b;
+   printf("sum = %d\n",c);
+}
+
+// Program End
---
+++ 05_functions/03_wr_na.c
@@ -0,0 +1,39 @@
+/*********************************************************************************************
+ * © 2022 RuggedBOARRD. - All Rights Reserved. Permission to use, modify, copy, and distribute
+ * this source code, object code, or executable code (collectively, Software), is granted only
+ * under the terms of a valid written license agreement with RuggedBOARD. Unauthorized copying
+ * or other use of the Software is strictly prohibited.  Software is owned by and constitutes
+ * the proprietary works, trade secrets, and copyrights of Embitel or its licensors.
+ * For further information, contact community.ruggedboard.com / info@ruggedboard.com
+ *
+ * Application: 
+ * Brief: 
+ * Author: Suraj Singh
+ * Title: Program Engineer
+ * Last Modified Date: 01.04.2023
+ *
+/*********************************************************************************************/
+
+// Header File
+#include<stdio.h>
+
+int sum (void);//prototype
+
+int main ()
+{
+   int c;
+   c= sum ();
+   printf("sum = %d\n",c);
+   return 0;
+}
+
+int sum ()//function declaration
+{
+   int a,b,c;
+   printf("enter 2 numbers:");
+   scanf ("%d%d", &a, &b);
+   c = a+b;
+   return c;
+}
+
+// Program End
